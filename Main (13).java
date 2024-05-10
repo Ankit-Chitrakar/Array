e02cb21ff68eb4243cea56a1566b100805f21024
Ankit Chitrakar <98268401+Ankit-Chitrakar@users.noreply.github.com>
2024-05-10 14:31:47 +0530

    ArrayList Introduction and methods

---
+++ Main (13).java
@@ -0,0 +1,80 @@
+/******************************************************************************
+
+    ArrayList
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+// 		int n = sc.nextInt();
+        ArrayList<String> list = new ArrayList<>();
+        
+        
+        // add
+        
+        list.add("Chitrakar");
+        list.add("Ankit");
+        
+        System.out.println(list);  // [Chitrakar, Ankit]
+		
+		list.add(1, "Tiasa");
+		list.add(3, "Chitrakar");
+		
+		System.out.println(list); // [Chitrakar, Tiasa, Ankit, Chitrakar]
+		
+		// delete
+		
+		list.remove("Chitrakar"); // delete first instance of this 
+		list.remove(1);  // delete index 1
+		
+		System.out.println(list);  // [Tiasa, Chitrakar]
+		
+		
+		// traverse
+		
+		for(int i = 0; i < list.size(); i++){
+		    System.out.print(list.get(i)+" ");    // Tiasa Chitrakar
+		}
+		
+		
+		// modify
+		
+		list.set(1, "Ash");
+		System.out.println(list);  // [Tiasa, Ash]
+		
+		
+		// sort
+		
+		Collections.sort(list);  
+		
+		System.out.println(list);  // [Ash, Tiasa]
+		
+		// modify through iteration
+		
+		for(int i = 0; i < list.size(); i++){
+		    if(list.contains("Ash")){
+		        list.set(i, "Chitra Kar");
+		    }
+		}
+		
+		System.out.println(list);   // [Chitra Kar, Tiasa]
+		
+		// remove all
+		
+		list.clear();
+		System.out.println(list);  // []
+		
+		// check list has no element or not
+		
+		if(list.isEmpty() == true){
+		    System.out.println("Yes");    // Yes
+		}
+		else{
+		    System.out.println("No");
+		}
+		
+		
+	}
+}
